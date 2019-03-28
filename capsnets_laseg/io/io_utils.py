bee5ff9724b947d7bb97d0a38cba8bfdde755d22
Joseph <jchen42703@gmail.com>
2019-03-28 19:20:31 -0400

    Added `isensee_preprocess` as a function
    This is to make the code for io.py 's LocalPreprocessingBinarySeg module and the inference module cleaner.

--- capsnets_laseg/io/io_utils.py
+++ capsnets_laseg/io/io_utils.py
@@ -1,6 +1,53 @@
 import numpy as np
 from skimage.transform import resize
 from keras_med_io.utils.intensity_io import clip_upper_lower_percentile
+from keras_med_io.utils.shape_io import extract_nonint_region, resample_array
+from nibabel import Nifti1Image
+
+def isensee_preprocess(input_image, mask, orig_spacing, get_coords = False, ct = False, mean_patient_shape = (115, 320, 232)):
+    """
+    Order:
+    1) Cropping to non-zero regions
+    2) Resampling to the median voxel spacing of the respective dataset
+    3) Normalization
+
+    Args:
+        input_image:
+        mask:
+        orig_spacing: list/numpy array of voxel spacings corresponding to each axis of input_image and mask (assumes they have the same spacings)
+            * If it is left as None, then the images will not be resampled.
+        get_coords: boolean on whether to return extraction coords or not
+        ct: boolean on whether `input_image` is a CT scan or not
+        mean_patient_shape: obtained from Table 1. in the nnU-Net paper
+    Returns:
+        preprocessed input image and mask
+    """
+    # converting types and axes order
+    if isinstance(input_image, Nifti1Image):
+        input_image = nii_to_np(input_image)
+    if isinstance(mask, Nifti1Image):
+        mask = nii_to_np(mask)
+    # 1. Cropping
+    if get_coords:
+        extracted_img, extracted_mask, coords = extract_nonint_region(input_image, mask = mask, outside_value = 0, coords = True)
+    elif not get_coords:
+        extracted_img, extracted_mask = extract_nonint_region(input_image, mask = mask, outside_value = 0, coords = False)
+    # 2. Resampling
+    if orig_spacing is None: # renaming the variables because they don't get resampled
+        resamp_img = extracted_img
+        resamp_label = extracted_mask
+    else:
+        transposed_spacing = orig_spacing[::-1] # doing so because turning into numpy array moves the batch dimension to axis 0
+        med_spacing = [np.median(transposed_spacing) for i in range(3)]
+        resamp_img = resample_array(extracted_img, transposed_spacing, med_spacing, is_label = False)
+        resamp_label = resample_array(extracted_mask, transposed_spacing, med_spacing, is_label = True)
+    # 3. Normalization
+    norm_img = zscore_isensee(resamp_img, ct = ct, mean_patient_shape = mean_patient_shape)
+    if get_coords:
+        return (norm_img, resamp_label, coords)
+    elif not get_coords:
+        return (norm_img, resamp_label)
+
 # Normalization FN
 def zscore_isensee(arr, ct, mean_patient_shape):
     """"
@@ -45,3 +92,9 @@ def zscore_norm(arr):
     arr = arr.flatten()
     norm_img = (arr-np.mean(arr)) / np.std(arr)
     return norm_img.reshape(shape)
+
+def nii_to_np(nib_img):
+    """
+    Converts a 3D nifti image to a numpy array of (z, x, y) dims
+    """
+    return np.transpose(nib_img.get_fdata(), [-1, 0, 1])
